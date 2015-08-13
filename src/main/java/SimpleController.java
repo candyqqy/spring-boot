1778deee22636bb5f185f574612ce05339e791b9
QQY <candy_dr_qqy@hotmail.com>
2015-08-13 16:38:06 +0800

    modify main controller

--- src/main/java/SimpleController.java
+++ src/main/java/SimpleController.java
@@ -1,7 +1,6 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,9 +13,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @EnableAutoConfiguration
 public class SimpleController {
 
-    @RequestMapping(value = "/weixin/{signature}/{timestamp}/{nonce}/{echostr}",method = RequestMethod.GET)
+    @RequestMapping(value = "/weixin",method = RequestMethod.GET,params = {"signature","timestamp","nonce","echostr"})
     @ResponseBody
-    public String check(@PathVariable String signature,@PathVariable String timestamp,@PathVariable String nonce,@PathVariable String echostr){
+    public String check(String signature,String timestamp,String nonce,String echostr){
 
         if (SignUtil.checkSignature(signature, timestamp, nonce)) {
             return echostr;
